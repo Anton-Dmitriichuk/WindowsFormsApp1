989cb5f976199832c1343792aa551781472c833f
Anton-Dmitriichuk <99538781+Anton-Dmitriichuk@users.noreply.github.com>
2023-11-17 13:08:09 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Contract_page = new System.Windows.Forms.TabPage();
+            this.client_date_of_birth = new System.Windows.Forms.TextBox();
+            this.add_client_button = new System.Windows.Forms.Button();
+            this.client_phone_text = new System.Windows.Forms.Label();
+            this.client_address_text = new System.Windows.Forms.Label();
+            this.client_date_of_birth_text = new System.Windows.Forms.Label();
+            this.client_patronymic_text = new System.Windows.Forms.Label();
+            this.client_num_passport_text = new System.Windows.Forms.Label();
+            this.client_contingent_text = new System.Windows.Forms.Label();
+            this.client_surname_text = new System.Windows.Forms.Label();
+            this.client_series_passport_text = new System.Windows.Forms.Label();
+            this.client_name_text = new System.Windows.Forms.Label();
+            this.client_phone = new System.Windows.Forms.TextBox();
+            this.client_address = new System.Windows.Forms.TextBox();
+            this.client_num_passport = new System.Windows.Forms.TextBox();
+            this.client_patronymic = new System.Windows.Forms.TextBox();
+            this.client_series_passport = new System.Windows.Forms.TextBox();
+            this.client_contingent = new System.Windows.Forms.TextBox();
+            this.client_surname = new System.Windows.Forms.TextBox();
+            this.client_name = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.Contract_page.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Contract_page);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1448, 519);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Contract_page
+            // 
+            this.Contract_page.Controls.Add(this.textBox1);
+            this.Contract_page.Controls.Add(this.client_date_of_birth);
+            this.Contract_page.Controls.Add(this.add_client_button);
+            this.Contract_page.Controls.Add(this.client_phone_text);
+            this.Contract_page.Controls.Add(this.client_address_text);
+            this.Contract_page.Controls.Add(this.client_date_of_birth_text);
+            this.Contract_page.Controls.Add(this.client_patronymic_text);
+            this.Contract_page.Controls.Add(this.client_num_passport_text);
+            this.Contract_page.Controls.Add(this.client_contingent_text);
+            this.Contract_page.Controls.Add(this.client_surname_text);
+            this.Contract_page.Controls.Add(this.client_series_passport_text);
+            this.Contract_page.Controls.Add(this.client_name_text);
+            this.Contract_page.Controls.Add(this.client_phone);
+            this.Contract_page.Controls.Add(this.client_address);
+            this.Contract_page.Controls.Add(this.client_num_passport);
+            this.Contract_page.Controls.Add(this.client_patronymic);
+            this.Contract_page.Controls.Add(this.client_series_passport);
+            this.Contract_page.Controls.Add(this.client_contingent);
+            this.Contract_page.Controls.Add(this.client_surname);
+            this.Contract_page.Controls.Add(this.client_name);
+            this.Contract_page.Location = new System.Drawing.Point(4, 22);
+            this.Contract_page.Name = "Contract_page";
+            this.Contract_page.Padding = new System.Windows.Forms.Padding(3);
+            this.Contract_page.Size = new System.Drawing.Size(1440, 493);
+            this.Contract_page.TabIndex = 0;
+            this.Contract_page.Text = "ы";
+            this.Contract_page.UseVisualStyleBackColor = true;
+            // 
+            // client_date_of_birth
+            // 
+            this.client_date_of_birth.Location = new System.Drawing.Point(404, 144);
+            this.client_date_of_birth.Name = "client_date_of_birth";
+            this.client_date_of_birth.Size = new System.Drawing.Size(174, 20);
+            this.client_date_of_birth.TabIndex = 4;
+            // 
+            // add_client_button
+            // 
+            this.add_client_button.Location = new System.Drawing.Point(306, 365);
+            this.add_client_button.Name = "add_client_button";
+            this.add_client_button.Size = new System.Drawing.Size(283, 56);
+            this.add_client_button.TabIndex = 3;
+            this.add_client_button.Text = "Добавить пациента";
+            this.add_client_button.UseVisualStyleBackColor = true;
+            this.add_client_button.Click += new System.EventHandler(this.add_client_button_Click);
+            // 
+            // client_phone_text
+            // 
+            this.client_phone_text.AutoSize = true;
+            this.client_phone_text.Location = new System.Drawing.Point(303, 316);
+            this.client_phone_text.Name = "client_phone_text";
+            this.client_phone_text.Size = new System.Drawing.Size(93, 13);
+            this.client_phone_text.TabIndex = 1;
+            this.client_phone_text.Text = "Номер телефона";
+            // 
+            // client_address_text
+            // 
+            this.client_address_text.AutoSize = true;
+            this.client_address_text.Location = new System.Drawing.Point(295, 290);
+            this.client_address_text.Name = "client_address_text";
+            this.client_address_text.Size = new System.Drawing.Size(103, 13);
+            this.client_address_text.TabIndex = 1;
+            this.client_address_text.Text = "Адрес проживания";
+            // 
+            // client_date_of_birth_text
+            // 
+            this.client_date_of_birth_text.AutoSize = true;
+            this.client_date_of_birth_text.Location = new System.Drawing.Point(312, 151);
+            this.client_date_of_birth_text.Name = "client_date_of_birth_text";
+            this.client_date_of_birth_text.Size = new System.Drawing.Size(86, 13);
+            this.client_date_of_birth_text.TabIndex = 1;
+            this.client_date_of_birth_text.Text = "Дата рождения";
+            // 
+            // client_patronymic_text
+            // 
+            this.client_patronymic_text.AutoSize = true;
+            this.client_patronymic_text.Location = new System.Drawing.Point(342, 89);
+            this.client_patronymic_text.Name = "client_patronymic_text";
+            this.client_patronymic_text.Size = new System.Drawing.Size(54, 13);
+            this.client_patronymic_text.TabIndex = 1;
+            this.client_patronymic_text.Text = "Отчество";
+            // 
+            // client_num_passport_text
+            // 
+            this.client_num_passport_text.AutoSize = true;
+            this.client_num_passport_text.Location = new System.Drawing.Point(307, 264);
+            this.client_num_passport_text.Name = "client_num_passport_text";
+            this.client_num_passport_text.Size = new System.Drawing.Size(91, 13);
+            this.client_num_passport_text.TabIndex = 1;
+            this.client_num_passport_text.Text = "Номер паспорта";
+            // 
+            // client_contingent_text
+            // 
+            this.client_contingent_text.AutoSize = true;
+            this.client_contingent_text.Location = new System.Drawing.Point(333, 115);
+            this.client_contingent_text.Name = "client_contingent_text";
+            this.client_contingent_text.Size = new System.Drawing.Size(65, 13);
+            this.client_contingent_text.TabIndex = 1;
+            this.client_contingent_text.Text = "Контингент";
+            // 
+            // client_surname_text
+            // 
+            this.client_surname_text.AutoSize = true;
+            this.client_surname_text.Location = new System.Drawing.Point(342, 63);
+            this.client_surname_text.Name = "client_surname_text";
+            this.client_surname_text.Size = new System.Drawing.Size(56, 13);
+            this.client_surname_text.TabIndex = 1;
+            this.client_surname_text.Text = "Фамилия";
+            // 
+            // client_series_passport_text
+            // 
+            this.client_series_passport_text.AutoSize = true;
+            this.client_series_passport_text.Location = new System.Drawing.Point(310, 238);
+            this.client_series_passport_text.Name = "client_series_passport_text";
+            this.client_series_passport_text.Size = new System.Drawing.Size(88, 13);
+            this.client_series_passport_text.TabIndex = 1;
+            this.client_series_passport_text.Text = "Серия паспорта";
+            // 
+            // client_name_text
+            // 
+            this.client_name_text.AutoSize = true;
+            this.client_name_text.Location = new System.Drawing.Point(369, 37);
+            this.client_name_text.Name = "client_name_text";
+            this.client_name_text.Size = new System.Drawing.Size(29, 13);
+            this.client_name_text.TabIndex = 1;
+            this.client_name_text.Text = "Имя";
+            // 
+            // client_phone
+            // 
+            this.client_phone.Location = new System.Drawing.Point(404, 313);
+            this.client_phone.Name = "client_phone";
+            this.client_phone.Size = new System.Drawing.Size(174, 20);
+            this.client_phone.TabIndex = 0;
+            // 
+            // client_address
+            // 
+            this.client_address.Location = new System.Drawing.Point(404, 287);
+            this.client_address.Name = "client_address";
+            this.client_address.Size = new System.Drawing.Size(174, 20);
+            this.client_address.TabIndex = 0;
+            // 
+            // client_num_passport
+            // 
+            this.client_num_passport.Location = new System.Drawing.Point(404, 261);
+            this.client_num_passport.Name = "client_num_passport";
+            this.client_num_passport.Size = new System.Drawing.Size(174, 20);
+            this.client_num_passport.TabIndex = 0;
+            // 
+            // client_patronymic
+            // 
+            this.client_patronymic.Location = new System.Drawing.Point(404, 86);
+            this.client_patronymic.Name = "client_patronymic";
+            this.client_patronymic.Size = new System.Drawing.Size(174, 20);
+            this.client_patronymic.TabIndex = 0;
+            // 
+            // client_series_passport
+            // 
+            this.client_series_passport.Location = new System.Drawing.Point(404, 235);
+            this.client_series_passport.Name = "client_series_passport";
+            this.client_series_passport.Size = new System.Drawing.Size(174, 20);
+            this.client_series_passport.TabIndex = 0;
+            // 
+            // client_contingent
+            // 
+            this.client_contingent.Location = new System.Drawing.Point(404, 112);
+            this.client_contingent.Name = "client_contingent";
+            this.client_contingent.Size = new System.Drawing.Size(174, 20);
+            this.client_contingent.TabIndex = 0;
+            // 
+            // client_surname
+            // 
+            this.client_surname.Location = new System.Drawing.Point(404, 60);
+            this.client_surname.Name = "client_surname";
+            this.client_surname.Size = new System.Drawing.Size(174, 20);
+            this.client_surname.TabIndex = 0;
+            // 
+            // client_name
+            // 
+            this.client_name.Location = new System.Drawing.Point(404, 34);
+            this.client_name.Name = "client_name";
+            this.client_name.Size = new System.Drawing.Size(174, 20);
+            this.client_name.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1440, 493);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(658, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1448, 519);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.Contract_page.ResumeLayout(false);
+            this.Contract_page.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Contract_page;
+        private System.Windows.Forms.Label client_phone_text;
+        private System.Windows.Forms.Label client_address_text;
+        private System.Windows.Forms.Label client_patronymic_text;
+        private System.Windows.Forms.Label client_num_passport_text;
+        private System.Windows.Forms.Label client_surname_text;
+        private System.Windows.Forms.Label client_series_passport_text;
+        private System.Windows.Forms.Label client_name_text;
+        private System.Windows.Forms.TextBox client_phone;
+        private System.Windows.Forms.TextBox client_address;
+        private System.Windows.Forms.TextBox client_num_passport;
+        private System.Windows.Forms.TextBox client_patronymic;
+        private System.Windows.Forms.TextBox client_series_passport;
+        private System.Windows.Forms.TextBox client_surname;
+        private System.Windows.Forms.TextBox client_name;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label client_date_of_birth_text;
+        private System.Windows.Forms.Label client_contingent_text;
+        private System.Windows.Forms.TextBox client_contingent;
+        private System.Windows.Forms.Button add_client_button;
+        private System.Windows.Forms.TextBox client_date_of_birth;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.OleDb;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private OleDbConnection connection = new OleDbConnection();
+        public Form1()
+        {
+            InitializeComponent();
+            connection.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\Users\\Антон Дмитрийчук\\Desktop\\интерфейсы\\WindowsFormsApp1\\Database1.mdb;Persist Security Info=True";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection = new OleDbConnection(ConfigurationManager.ConnectionStrings["TestDB"].ConnectionString);
+
+            connection.Open();
+
+            if ( connection.State == ConnectionState.Open )
+            {
+                MessageBox.Show("Подключился");
+            } 
+            else
+            {
+                MessageBox.Show("NO Подключился");
+            }
+        }
+
+        private void add_client_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                OleDbCommand command_add_client = new OleDbCommand();
+                command_add_client.Connection = connection;
+                command_add_client.CommandText = "INSERT INTO Пациенты(Код_пациента, Имя, Фамилия, Отчество, Контингент," +
+                    "Дата_рождения, Серия_паспорта, Номер_паспорта, Адрес_проживания, Номер_телефона)" +
+                    "VALUES(@id, @name, @familia, @otchestvo, @contingent, @dob, @sp, @np, @address, @phone)";
+                connection.Open();
+                command_add_client.Parameters.Add("@id", OleDbType.SmallInt).Value = textBox1.Text;
+                command_add_client.Parameters.Add("@name", OleDbType.WChar).Value = client_name.Text;
+                command_add_client.Parameters.Add("@familia", OleDbType.WChar).Value = client_surname.Text;
+                command_add_client.Parameters.Add("@otchestvo", OleDbType.WChar).Value = client_patronymic.Text;
+                command_add_client.Parameters.Add("@contingent", OleDbType.WChar).Value = client_contingent.Text;
+                command_add_client.Parameters.Add("@dob", OleDbType.Date).Value = client_date_of_birth.Text;
+                command_add_client.Parameters.Add("@sp", OleDbType.SmallInt).Value = client_series_passport.Text;
+                command_add_client.Parameters.Add("@np", OleDbType.SmallInt).Value = client_num_passport.Text;
+                command_add_client.Parameters.Add("@address", OleDbType.WChar).Value = client_address.Text;
+                command_add_client.Parameters.Add("@phone", OleDbType.WChar).Value = client_phone.Text;
+                command_add_client.ExecuteNonQuery();
+                connection.Close();
+                
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("err" + ex);
+            }
+            
+        }
+    }
+}
+
+
+//{Convert.ToInt32(textBox1.Text)}
+
+/*
+public partial class Form1 : Form
+{
+    private OleDbConnection connection = null;
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        connection = new OleDbConnection(ConfigurationManager.ConnectionStrings["TestDB"].ConnectionString);
+
+        connection.Open();
+
+        if (connection.State == ConnectionState.Open)
+        {
+            MessageBox.Show("Подключился");
+        }
+        else
+        {
+            MessageBox.Show("NO Подключился");
+        }
+    }
+
+    private void add_client_button_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            OleDbCommand command_add_client = new OleDbCommand($"INSERT INTO Пациенты (Код_пациента, Имя, Фамилия, Отчество, " +
+                            "Контингент, Дата_рождения, Серия_паспорта, Номер_паспорта, Адрес_проживания, Номер_телефона)" +
+                            $"VALUES ({textBox1.Text}, {client_name.Text}, {client_surname.Text}, " +
+                            $"{client_patronymic.Text}, {client_contingent.Text}, {client_date_of_birth.Text}, " +
+                            $"{client_series_passport.Text}, " +
+                            $"{client_num_passport.Text}, {client_address.Text}, {client_phone.Text})", connection);
+
+
+
+            command_add_client.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("err" + ex);
+        }
+
+    }
+}
+*/
+
+
+
+/*
+    connection.Open();
+    OleDbCommand command_add_client = new OleDbCommand();
+    command_add_client.Connection = connection;
+    command_add_client.CommandText = "INSERT INTO Пациенты (Имя, Фамилия, Отчество, Контингент, Дата_рождения, Серия_паспорта, Номер_паспорта, Адрес_проживания, Номер_телефона) VALUES('"+ client_name.Text +"','"+ client_surname.Text +"','" + client_patronymic.Text+"','"+client_contingent.Text+"','"+client_date_of_birth.Text+"','" +client_series_passport.Text+"','"+client_num_passport.Text+"','"+client_address.Text+"','"+client_phone.Text+"')";
+
+
+    command_add_client.ExecuteNonQuery();
+    connection.Close(); 
+ */
